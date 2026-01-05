2c8cde8175f8aa3a6382d674ae38bf7b57337bce
yangchaoliao <liaoyangchao1991@qq.com>
2026-01-05 16:55:29 +0000

    Deploying to gh-pages from @ yangchaoliao/researchlab@35b3205131dc07e59d85f844213f87f9cf5c1152 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-publications",
           title: "Publications",
-          description: "Representative ten recent publications.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/researchlab/publications/";
